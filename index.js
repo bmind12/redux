dc634704241d3ec52ab140ddd2be1d35263819f4
Sergey <ryabovsergey12@gmail.com>
2020-09-03 04:52:22 +0200

    makes it work, dirty solution

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
-function createStore(reducer) {
-  // TODO: , [preloadedState], [enhancer]
-  const store = {
-    reducer: 0, // TODO: change
-  };
+function createStore(reducer, preloadedState = {}) {
+  // TODO: [enhancer]
+  const store = preloadedState;
+  const reducers = reducer;
 
   return {
     getState: () => store,
     dispatch: (action) => {
-      store.reducer = reducer(store.reducer, action);
+      Object.entries(reducers).forEach((reducer) => {
+        store[reducer[0]] = reducer[1](store[reducer[0]], action);
+      });
     },
     // subscribe, // subscribe(listener)
     // replaceReducer, // replaceReducer(nextReducer)
   };
 }
-// function combineReducers(reducers) {}
+
+function combineReducers(reducers) {
+  return reducers;
+}
 // function applyMiddleware(...middlewares) {}
 // function bindActionCreators(actionCreators, dispatch) {}
 // function compose(...functions) {}
 
-const { getState, dispatch } = createStore(counter);
+const reducers = combineReducers({
+  counter,
+  input,
+});
 
+const { getState, dispatch } = createStore(reducers);
+
+dispatch({ type: "INCREMENT" });
 console.log(getState());
 dispatch({ type: "INCREMENT" });
+dispatch({ type: "CHANGE", payload: "hello" });
+console.log(getState());
+dispatch({ type: "DECREMENT" });
+dispatch({ type: "RESET" });
 console.log(getState());
 
 function counter(state = 0, action) {
@@ -30,6 +44,17 @@ function counter(state = 0, action) {
       return state + 1;
     case "DECREMENT":
       return state - 1;
+    default:
+      return state;
+  }
+}
+
+function input(state = "", action) {
+  switch (action.type) {
+    case "CHANGE":
+      return action.payload;
+    case "RESET":
+      return "";
     default:
       return state;
   }
